1d7d59548897cbde1a1e8aacd957ad965bbb4905
温酒 <zjwjxdm@gmail.com>
2018-03-23 13:20:39 +0800

    第四题，分治，割，二分法

--- solutions/4/solution.py
+++ solutions/4/solution.py
@@ -30,6 +30,49 @@ def find_median(numsa, numsb):
     return ret
 
 
+def find_median_optimized(lista, listb):
+    lena, lenb = len(lista), len(listb)
+    if lena > lenb:
+        lista, listb, lena, lenb = listb, lista, lenb, lena
+
+    if lenb == 0:
+        raise ValueError
+
+    cut_a_min = 0
+    cut_a_max = lena
+    half_len = int((lena+lenb+1)/2)
+    while cut_a_min <= cut_a_max:
+        cut_a_try = int((cut_a_min + cut_a_max)/2)
+        cut_b_try = half_len - cut_a_try
+
+        if cut_a_try < cut_a_max and listb[cut_b_try-1] > lista[cut_a_try]:
+            cut_a_min = cut_a_try + 1
+        elif cut_a_try > 0 and lista[cut_a_try-1] > listb[cut_b_try]:
+            cut_a_max = cut_a_try - 1
+        else:
+            max_of_left = 0
+            min_of_right = 0
+            if cut_a_try == 0:
+                max_of_left = listb[cut_b_try-1]
+            elif cut_b_try == 0:
+                max_of_left = lista[cut_a_try-1]
+            else:
+                max_of_left = max(lista[cut_a_try-1], listb[cut_b_try-1])
+
+            if (lena + lenb) % 2 == 1:
+                return max_of_left
+
+            if cut_a_try == lena:
+                min_of_right = listb[cut_b_try]
+            elif cut_b_try == lenb:
+                min_of_right = lista[cut_a_try]
+            else:
+                min_of_right = min(lista[cut_a_try], listb[cut_b_try])
+
+            return (max_of_left + min_of_right) / 2.0
+
+
 if __name__ == '__main__':
-    print(find_median([1, 3], [2]))
-    print(find_median([1, 2], [3, 4]))
+    print(find_median_optimized([1, 3], [2]))
+    print('-------------')
+    print(find_median_optimized([1, 2], [3, 4]))
